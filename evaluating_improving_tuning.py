d12efb356785d1480640f4864664753eaebd49d6
Felipe Hargreaves <fhargreaves00@gmail.com>
2019-04-05 01:12:42 -0300

    Modify activation functions.

--- evaluating_improving_tuning.py
+++ evaluating_improving_tuning.py
@@ -103,7 +103,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 def build_classifier():
     classifier = Sequential()
-    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'tanh', input_dim = 11))
     classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
     classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
     classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
