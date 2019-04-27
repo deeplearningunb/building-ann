e250379473171fb4573684344d78798f29f50476
LucasPenido <lpenidoa@me.com>
2019-04-26 23:24:26 -0300

    Tentativa falha de utilizar o Talos

--- evaluating_improving_tuning.py
+++ evaluating_improving_tuning.py
@@ -15,9 +15,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import talos as ta
 
 # Importing the dataset
-dataset = pd.read_csv('Churn_Modelling.csv')
+dataset = pd.read_csv('/home/lucas/Documentos/UnB/DeepLearning/building-ann/Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
@@ -54,17 +55,17 @@ classifier = Sequential()
 
 # Adding the input layer and the first hidden layer
 classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
-# classifier.add(Dropout(p = 0.1))
+# classifier.add(Dropout(rate = 0.1))
 
 # Adding the second hidden layer
 classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
-# classifier.add(Dropout(p = 0.1))
+# classifier.add(Dropout(rate = 0.1))
 
 # Adding the output layer
 classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
 
 # Compiling the ANN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer = 'nadam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 # Fitting the ANN to the Training set
 classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
@@ -101,14 +102,17 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
 from keras.models import Sequential
 from keras.layers import Dense
+#from keras.layers import Dropout
 def build_classifier():
     classifier = Sequential()
     classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    classifier.add(Dropout(rate = 0.1))
     classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dropout(rate = 0.1))
     classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
     classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
     return classifier
-classifier = KerasClassifier(build_fn = build_classifier, batch_size = 10, epochs = 100)
+classifier = KerasClassifier(build_fn = build_classifier, batch_size = 10, epochs = 10)
 accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10, n_jobs = -1)
 mean = accuracies.mean()
 variance = accuracies.std()
@@ -121,21 +125,127 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import GridSearchCV
 from keras.models import Sequential
 from keras.layers import Dense
+
 def build_classifier(optimizer):
     classifier = Sequential()
     classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    classifier.add(Dropout(rate = 0.1))
     classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dropout(rate = 0.1))
     classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
     classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
     return classifier
+
 classifier = KerasClassifier(build_fn = build_classifier)
+
 parameters = {'batch_size': [25, 32],
-              'epochs': [100, 500],
+              'epochs': [100, 250, 500],
               'optimizer': ['adam', 'rmsprop']}
+
 grid_search = GridSearchCV(estimator = classifier,
-                           param_grid = parameters,
                            scoring = 'accuracy',
-                           cv = 10)
+                           param_grid = parameters,
+                           cv = 10,
+                           n_jobs = -1)
+
 grid_search = grid_search.fit(X_train, y_train)
+
 best_parameters = grid_search.best_params_
 best_accuracy = grid_search.best_score_
+
+# Using Talos to tuning ANN
+
+import pandas as pd
+import talos as ta
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from talos.model.normalizers import lr_normalizer
+from talos.metrics.keras_metrics import fmeasure_acc
+from keras.losses import binary_crossentropy, logcosh
+from keras.optimizers import Adam, Nadam, RMSprop 
+from talos import live
+
+dataset = pd.read_csv('/home/lucas/Documentos/UnB/DeepLearning/building-ann/Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+import wrangle as wr
+# then we load the dataset
+x, y = ta.datasets.breast_cancer()
+
+# and normalize every feature to mean 0, std 1
+x = wr.mean_zero(pd.DataFrame(x)).values
+
+
+def build_classifierTalos(x_train, y_train, x_val, y_val, params):
+    classifier = Sequential()
+    classifier.add(Dense(units = 6, 
+                         kernel_initializer = 'uniform', 
+                         activation = params['activation'], 
+                         input_dim = x_train.shape[1]))
+    
+    classifier.add(Dropout(params['dropout']))
+    
+    classifier.add(Dense(units = 6, 
+                         kernel_initializer = 'uniform', 
+                         activation = params['activation']))
+    
+    classifier.add(Dropout(params['dropout']))
+    
+    classifier.add(Dense(units = 1, 
+                         kernel_initializer = 'uniform', 
+                         activation = params['last_activation']))
+    
+    # here we add a regulizer normalization function from Talos
+    classifier.compile(optimizer = params['optimizer'](lr=lr_normalizer(params['lr'],params['optimizer'])), 
+                       loss = params['losses'], 
+                       metrics = ['acc', fmeasure_acc])
+    
+    history = classifier.fit(x_train, y_train, 
+                        validation_data=[x_val, y_val],
+                        batch_size=params['batch_size'],
+                        callbacks=[live()],
+                        epochs=params['epochs'],
+                        verbose=0)
+    
+    return history, classifier
+
+parametes = {'lr': (0.5, 5, 10),
+     'first_neuron':[4, 8, 16, 32, 64],
+     'batch_size': (25, 32),
+     'epochs': [200, 250],
+     'dropout': (0, 0.1),
+     'optimizer': [Adam, Nadam, RMSprop],
+     'losses': [logcosh, binary_crossentropy],
+     'activation':['relu', 'elu'],
+     'kernel_initializer':['uniform', 'normal'],
+     'last_activation': ['sigmoid']}
+
+v = ta.datasets.iris()
+print(v);
+t = ta.Scan(X_train,
+            y_train,
+            model=build_classifierTalos,
+            params=parametes)
