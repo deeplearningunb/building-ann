cf27c907def39e5684019d18ef496cbe013463db
jonathan jorge <jonathanjorge8445@gmail.com>
2020-09-18 23:35:06 -0300

    change  activation function

--- ann.py
+++ ann.py
@@ -23,12 +23,23 @@ y = dataset.iloc[:, 13].values
 
 # Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
 labelencoder_X_1 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
 labelencoder_X_2 = LabelEncoder()
 X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-onehotencoder = OneHotEncoder(categorical_features = [1])
-X = onehotencoder.fit_transform(X).toarray()
+onehotencoder =ColumnTransformer(
+    transformers=[
+        ("OneHot",        
+         OneHotEncoder(), 
+         [1]             
+         )
+    ],
+    remainder='passthrough' 
+)
+
+X = onehotencoder.fit_transform(X)
+# a função OneHotEncoder estava dando erro entao mudei a implementação da mesma
 X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
@@ -52,19 +63,24 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Adding the input layer and the first hidden layer
-classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'tanh', input_dim = 11))
 
 # Adding the second hidden layer
 classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
 
 # Adding the output layer
 classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+# Mudei as funções de ativações percebi que quando a função de ativacao são todas relu a acuracia varia mais,
+# porém o resultado final é o quase o mesmo de anteriormente
+# Não percebi muita diferença nos resultados quando alterados as funções de ativações, algumas sequencias deixou a acuracia
+# inconstante fazendo varia de 0.85 á 0.86 a cada epoch depois do cotovelo  
+
 
 # Compiling the ANN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 # Fitting the ANN to the Training set
-classifier.fit(X_train, y_train, batch_size = 10, epochs = 30)
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 50)
 
 # Part 3 - Making predictions and evaluating the model
 
@@ -75,3 +91,4 @@ y_pred = (y_pred > 0.5)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+print(cm)
